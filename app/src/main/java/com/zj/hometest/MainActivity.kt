6ac4b7163d72cb9aed1fcfb0c37e04c7ccb0d2af
zbynekjuros <zbynekjuros@gmail.com>
2025-07-18 22:27:16 +0200

    removed unused

--- app/src/main/java/com/zj/hometest/MainActivity.kt
+++ app/src/main/java/com/zj/hometest/MainActivity.kt
@@ -22,8 +22,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        val window = window
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
